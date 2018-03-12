bffdbc73b6794be1cc5ac36f03a3b28b7892c852
Ivan Malopinsky <imsky@users.noreply.github.com>
2018-03-12 13:37:35 -0400

    sketch out job comparison

--- main.go
+++ main.go
@@ -16,23 +16,23 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
+	v1 "k8s.io/client-go/pkg/api/v1"
 	batchv1 "k8s.io/client-go/pkg/apis/batch/v1"
+	batchv2alpha1 "k8s.io/client-go/pkg/apis/batch/v2alpha1"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-//todo: get remote jobs by label
-// --
-//todo: clean up JobSpec, removing any non-comparable fields
 //todo: compare clean JobSpecs, detect difference
+// --
 //todo: generate plan to deal with difference: create, update, delete
 //todo: perform plan, confirm plan has succeeded
+// --
 //todo: convert panic() calls to log errors in a structured way
-
-/*
-should fail if more than one resource is matched on selector (for some resources?)
-should also fail if the source resources don't match selector
-job cronjob parents can be looked up by metadata.ownerReferences (use uid)
-*/
+//todo: should fail if more than one resource is matched on selector (for some resources?)
+//todo: should also fail if the source resources don't match selector
+//todo: get field names from json tags
+//todo: consider comparing json (or deserialized json) instead of direct fields
+//todo: consider implementing visitor pattern similar to kubectl for comparisons
 
 func isAcceptedGroupVersionKind(gvk schema.GroupVersionKind) bool {
 	acceptedGroupVersionKinds := []schema.GroupVersionKind{
@@ -109,7 +109,7 @@ func validateObjects(objects []runtime.Object) error {
 
 func getObjectMetadata(o runtime.Object) (metav1.Object, apilabels.Set) {
 	switch t := o.(type) {
-	case *batchv1.Job:
+	default:
 		metadata, err := meta.Accessor(t)
 
 		if err != nil {
@@ -150,6 +150,7 @@ func pairObjectsByCriteria(srcObjects []runtime.Object, dstObjects []runtime.Obj
 
 	for _, src := range srcObjects {
 		srcMetadata, srcLabels := getObjectMetadata(src)
+		pair := ObjectPair{&src, nil}
 
 		for _, dst := range dstObjects {
 			dstMetadata, dstLabels := getObjectMetadata(dst)
@@ -157,12 +158,176 @@ func pairObjectsByCriteria(srcObjects []runtime.Object, dstObjects []runtime.Obj
 			if criteria.label != "" &&
 				srcLabels.Get(criteria.label) == dstLabels.Get(criteria.label) &&
 				srcMetadata.GetNamespace() == dstMetadata.GetNamespace() {
-				pairs = append(pairs, ObjectPair{&src, &dst})
+				pair.dst = &dst
+				pairs = append(pairs, pair)
 			}
 		}
 	}
 
 	return pairs
+}
+
+func deepCompareObject(src runtime.Object, dst runtime.Object) []string {
+	var fields []string
+
+	//todo: bail if src/dst types are different
+
+	switch srcType := src.(type) {
+	case *batchv1.Job:
+		dstJob := dst.(*batchv1.Job)
+		return deepCompareJobSpec(srcType.Spec, dstJob.Spec)
+	case *batchv2alpha1.CronJob:
+		dstCronJob := dst.(*batchv2alpha1.CronJob)
+		return deepCompareCronJobSpec(srcType.Spec, dstCronJob.Spec)
+	}
+
+	return fields
+}
+
+func deepCompareCronJobSpec(src batchv2alpha1.CronJobSpec, dst batchv2alpha1.CronJobSpec) []string {
+	var fields []string
+
+	if src.Schedule != dst.Schedule {
+		fields = append(fields, "schedule")
+	}
+
+	if src.ConcurrencyPolicy != dst.ConcurrencyPolicy {
+		fields = append(fields, "schedule")
+	}
+
+	if src.Suspend != nil {
+		if dst.Suspend == nil || *src.Suspend != *dst.Suspend {
+			fields = append(fields, "suspend")
+		}
+	}
+
+	if src.SuccessfulJobsHistoryLimit != nil {
+		if dst.SuccessfulJobsHistoryLimit == nil || *src.SuccessfulJobsHistoryLimit != *dst.SuccessfulJobsHistoryLimit {
+			fields = append(fields, "successfulJobsHistoryLimit")
+		}
+	}
+
+	if src.FailedJobsHistoryLimit != nil {
+		if dst.FailedJobsHistoryLimit == nil || *src.FailedJobsHistoryLimit != *dst.FailedJobsHistoryLimit {
+			fields = append(fields, "failedJobsHistoryLimit")
+		}
+	}
+
+	fields = append(fields, deepCompareJobTemplateSpec(src.JobTemplate, dst.JobTemplate)...)
+
+	return fields
+}
+
+func deepCompareJobTemplateSpec(src batchv2alpha1.JobTemplateSpec, dst batchv2alpha1.JobTemplateSpec) []string {
+	var fields []string
+
+	fields = append(fields, deepCompareJobSpec(src.Spec, dst.Spec)...)
+
+	return fields
+}
+
+func deepCompareJobSpec(src batchv1.JobSpec, dst batchv1.JobSpec) []string {
+	var fields []string
+
+	if src.ActiveDeadlineSeconds != nil {
+		if dst.ActiveDeadlineSeconds == nil || *src.ActiveDeadlineSeconds != *dst.ActiveDeadlineSeconds {
+			fields = append(fields, "activeDeadlineSeconds")
+		}
+	}
+
+	fields = append(fields, deepComparePodTemplateSpec(src.Template, dst.Template)...)
+
+	return fields
+}
+
+func deepComparePodTemplateSpec(src v1.PodTemplateSpec, dst v1.PodTemplateSpec) []string {
+	var fields []string
+
+	fields = append(fields, deepComparePodSpec(src.Spec, dst.Spec)...)
+
+	return fields
+}
+
+func deepComparePodSpec(src v1.PodSpec, dst v1.PodSpec) []string {
+	var fields []string
+
+	if src.RestartPolicy != dst.RestartPolicy {
+		fields = append(fields, "restartPolicy")
+	}
+
+	if src.TerminationGracePeriodSeconds != nil {
+		if dst.TerminationGracePeriodSeconds == nil || *src.TerminationGracePeriodSeconds != *dst.TerminationGracePeriodSeconds {
+			fields = append(fields, "terminationGracePeriodSeconds")
+		}
+	}
+
+	if src.ActiveDeadlineSeconds != nil {
+		if dst.ActiveDeadlineSeconds == nil || *src.ActiveDeadlineSeconds != *dst.ActiveDeadlineSeconds {
+			fields = append(fields, "activeDeadlineSeconds")
+		}
+	}
+
+	if len(compareNodeSelector(src.NodeSelector, dst.NodeSelector)) > 0 {
+		fields = append(fields, "nodeSelector")
+	}
+
+	if compareContainerArray(src.Containers, dst.Containers) {
+		fields = append(fields, "containers")
+	}
+
+	return fields
+}
+
+func compareNodeSelector(src map[string]string, dst map[string]string) []string {
+	var fields []string
+
+	for srcKey, srcVal := range src {
+		diff := true
+		if dstVal, ok := dst[srcKey]; ok {
+			if srcVal == dstVal {
+				diff = false
+			}
+		}
+
+		if diff {
+			fields = append(fields, srcKey)
+		}
+	}
+
+	return fields
+}
+
+//todo: maybe refactor to return actually differing fields
+func compareContainerArray(src []v1.Container, dst []v1.Container) bool {
+	if len(src) != len(dst) {
+		return true
+	}
+
+	for _, srcContainer := range src {
+		for _, dstContainer := range dst {
+			var foundMatchingContainer bool
+			if dstContainer.Name == srcContainer.Name {
+				foundMatchingContainer = true
+
+				//todo: compare env vars
+				if srcContainer.Image != dstContainer.Image {
+					return true
+				} else if srcContainer.WorkingDir != dstContainer.WorkingDir {
+					return true
+				} else if strings.Join(srcContainer.Command, " ") != strings.Join(dstContainer.Command, " ") {
+					return true
+				} else if strings.Join(srcContainer.Args, " ") != strings.Join(dstContainer.Args, " ") {
+					return true
+				}
+			}
+
+			if foundMatchingContainer == false {
+				return true
+			}
+		}
+	}
+
+	return false
 }
 
 func getObjectNamespaces(objects []runtime.Object) []string {
@@ -259,7 +424,8 @@ func main() {
 	pairs := pairObjectsByCriteria(srcObjects, dstObjects, PairCriteria{*label})
 
 	for _, pair := range pairs {
-		fmt.Println(*pair.src)
-		fmt.Println(*pair.dst)
+		if pair.src != nil && pair.dst != nil {
+			fmt.Println(deepCompareObject(*pair.src, *pair.dst))
+		}
 	}
 }
